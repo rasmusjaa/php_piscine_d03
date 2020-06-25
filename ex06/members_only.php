6a2a50cc28fed88f4965ec69e7108b98afdedad0
rasmusjaa <rasmus@lense.fi>
2020-06-25 21:37:10 +0300

    cleaned quotations

--- ex06/members_only.php
+++ ex06/members_only.php
@@ -1,14 +1,14 @@
 <?php
 if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "Ilovemylittleponey")
 {
-    $file = '../img/42.png';
+    $file = "../img/42.png";
     $img = base64_encode(file_get_contents($file));
     echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,$img'>\n</body></html>\n";
 }
 else
 {
     header("WWW-Authenticate: Basic realm=''Member area''");
-    header('HTTP/1.0 401 Unauthorized');
+    header("HTTP/1.0 401 Unauthorized");
 	echo "<html><body>That area is accessible for members only</body></html>     \n";
 }
 ?>
